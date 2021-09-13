155fc18750d7385010207b693d7376824e46146d
Gilbert J. Camarillo III <gil.camarillo73@gmail.com>
2021-09-13 09:55:54 -0500

    Week 3 Worksop Complete.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
-$(function() {
-    $(".carousel").carousel( { interval: 2000 } );
-    $("#carouselButton").click(function(){
+
+$(function () {
+    $(".carousel").carousel({ interval: 2000 });
+    $("#carouselButton").click(function () {
         if ($("#carouselButton").children("i").hasClass("fa-pause")) {
             $(".carousel").carousel("pause");
             $("#carouselButton").children("i").removeClass("fa-pause");
@@ -8,7 +9,21 @@ $(function() {
         } else {
             $(".carousel").carousel("cycle");
             $("#carouselButton").children("i").removeClass("fa-play");
-            $("#carouselButton").children("i").addClass("fa-pause"); 
+            $("#carouselButton").children("i").addClass("fa-pause");
         }
     });
 });
+
+
+// modal launch from button click
+$("#reserveButton").click(function () {
+
+    // show Modal
+    $('#reserveModal').modal('show');
+});
+$("#loginButton").click(function () {
+
+    // show Modal
+    $('#loginModal').modal('show');
+});
+
